2f120538ecf353eb582d538f3995d3ecf46f3945
unknown <nguyenquangkhai@gmail.com>
2018-05-04 09:53:28 +0700

    fix bug home-page

--- js/home-page.js
+++ js/home-page.js
@@ -1,24 +1,55 @@
 var shoppingCart = angular.module("shoppingCart", []);
 
-shoppingCart.controller("navbar", function($scope){
+shoppingCart.controller("navbar", function ($scope, $rootScope,$timeout) {
     $scope.display = "dsHidden";
     $scope.textColor = "text-white";
     $scope.icon = "navbar-toggler-icon-white";
 
-    $scope.controlMiniBar = function(){
-        if($scope.display == "dsHidden"){
+    $rootScope.$on("clearFromParent", function () {
+        $scope.clearColor();
+        $scope.clearDropDown();
+    });
+
+    $scope.controlMiniBar = function () {
+        if ($scope.display == "dsHidden") {
             $scope.bgColorNav = "bg-white";
             $scope.display = "dsBlock";
-            $scope.textColor = "text-dark";
-            $scope.bgColorCount = "bg-dark text-white";
             $scope.icon = "navbar-toggler-icon";
+            $scope.addColor();
         }
-        else{
+        else {
             $scope.display = "dsHidden";
             $scope.bgColorNav = " ";
-            $scope.textColor = "text-white";
-            $scope.bgColorCount = " ";
             $scope.icon = "navbar-toggler-icon-white";
+            $scope.clearColor();
         }
+    }
+
+    $scope.clearColor = function (){
+        $scope.textColor = "text-white";
+        $scope.linkColor = " ";
+        $scope.bgColorCount = " ";
+    }
+
+    $scope.addColor = function (){
+        $scope.textColor = "text-dark";
+        $scope.linkColor = "text-dark";
+        $scope.bgColorCount = "bg-dark text-white";
+    }
+
+    $scope.clearDropDown = function(){
+        $scope.choosedInfo = " ";
+        $scope.clearColor();
+    };
+    $scope.chooseInfo = function (choose){
+        $scope.choosedInfo = choose;
+        $scope.addColor();
+    }
+})
+
+shoppingCart.controller("window", function ($scope, $rootScope, $window) {
+    $scope.clearColorNav = function () {
+        if($window.innerWidth > 991)
+            $rootScope.$emit("clearFromParent", {});
     }
 });
